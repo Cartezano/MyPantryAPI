c23005c473582b13cff662708c3464c7605721d0
Hugo Páez <hapj@outlook.cl>
2017-08-27 02:01:32 -0300

    ADD MIGRATION DATABASE

---
+++ database/migrations/0000_00_00_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_type_id')->unsigned();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('password');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
---
+++ database/migrations/0000_00_00_000001_create_foreign_key_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForeignKeyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('user_type_id')
+                ->references('id')->on('user_types')
+                ->onDelete('restrict');
+        });
+
+        Schema::table('pantries', function (Blueprint $table) {
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('restrict');
+            $table->foreign('product_id')
+                ->references('id')->on('products')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_user_type_id_foreign');
+        });
+
+        Schema::table('pantries', function (Blueprint $table) {
+            $table->dropForeign('pantries_user_id_foreign');
+            $table->dropForeign('pantries_product_id_foreign');
+        });
+    }
+}
