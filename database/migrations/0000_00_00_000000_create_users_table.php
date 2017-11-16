c689a314b6a3b3b9ccdf5c75689ac357a4ae68a4
Hugo Páez <hapj@outlook.cl>
2017-11-16 00:31:11 -0300

    Elimina campo api_token del modelo usuario

--- database/migrations/0000_00_00_000000_create_users_table.php
+++ database/migrations/0000_00_00_000000_create_users_table.php
@@ -20,8 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('api_token', 60)->unique();
-            $table->rememberToken();
             
             $table->timestamps();
             $table->softDeletes();
